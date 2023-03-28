89abcea50522c62b900535f3a8070f100ef325af
Chau Quoc Khai <Khai.cq.27@gmail.com>
2023-03-28 13:17:28 +0700

    init nodejs

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// setup view engine
+configViewEngine(app);
+// init web route
+initWebRoute(app);
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
